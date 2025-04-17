95c89c8e5db23ef4a0c53587487f17b8b0bf3450
Alina <alinavik93@gmail.com>
2025-04-17 19:50:11 +0500

    Практическое задание - Collection - фильтрация

---
+++ src/app/javacode/Filter.java
@@ -0,0 +1,5 @@
+package app.javacode;
+
+public interface Filter {
+    Object apply(Object o);
+}
--- src/app/javacode/Main.java
+++ src/app/javacode/Main.java
@@ -1,7 +1,29 @@
 package app.javacode;
 
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String[] str = {"kdsjUSH", "HyscnkHk", "HKSNosmcl"};
+        Filter f = o -> o.toString().toLowerCase();
+        Object[] filteredStr = filter(str, f);
+        for (Object o : filteredStr) {
+            System.out.println(o);
+        }
+
+        Integer[] nums = {1, 2, 3, 4, 5};
+        Filter f2 = o -> (Integer) o * (Integer) o;
+        Integer[] nums2 = filter(nums, f2);
+        for (Object o : nums2) {
+            System.out.println(o);
+        }
+    }
+
+    public static <T> T[] filter(T[] array, Filter filter) {
+        T[] filtered = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length);
+        for (int i = 0; i < array.length; i++) {
+            filtered[i] = (T) filter.apply(array[i]);
+        }
+        return filtered;
     }
 }
