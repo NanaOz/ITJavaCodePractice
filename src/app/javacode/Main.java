38f6f91f87f5d4893da93faa25d5e8c7db22e8a7
Alina <alinavik93@gmail.com>
2025-04-17 20:17:15 +0500

    Практическое задание - Collection - count of elements

--- src/app/javacode/Main.java
+++ src/app/javacode/Main.java
@@ -1,7 +1,23 @@
 package app.javacode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Integer[] numbers = {1, 2, 1, 2, 3};
+        Map<Integer, Integer> map = countOccurrences(numbers);
+        System.out.println(map);
+
+    }
+
+    public static <T> Map<T, Integer>  countOccurrences(T[] array){
+        Map<T, Integer> map = new HashMap<>();
+
+        for (T t : array) {
+            map.merge(t, 1, Integer::sum);
+        }
+
+        return map;
     }
 }
