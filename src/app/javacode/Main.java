865eecf79a2e3b76bdf5abb8ea1008a388bffc76
Alina <alinavik93@gmail.com>
2025-04-17 16:43:15 +0500

    Практическое задание - StringBuilder

--- src/app/javacode/Main.java
+++ src/app/javacode/Main.java
@@ -2,6 +2,24 @@ package app.javacode;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MyStringBuilder stringBuilder = new MyStringBuilder();
+        stringBuilder.append("Onsjd");
+        stringBuilder.append("dlvos!");
+        System.out.println(stringBuilder);
+
+        stringBuilder.undo();
+        System.out.println(stringBuilder);
+
+        stringBuilder.append("twotwowtwo");
+        stringBuilder.append("three");
+        stringBuilder.append("focaodj");
+        System.out.println(stringBuilder);
+
+        stringBuilder.undo();
+        System.out.println(stringBuilder);
+
+        stringBuilder.undo();
+        System.out.println(stringBuilder);
+
     }
 }
---
+++ src/app/javacode/MyStringBuilder.java
@@ -0,0 +1,55 @@
+package app.javacode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStringBuilder {
+    private StringBuilder stringBuilder;
+    private final List<String> snapshot;
+    private int currentSnapshotIndex;
+
+    public MyStringBuilder() {
+        this.stringBuilder = new StringBuilder();
+        this.snapshot = new ArrayList<>();
+        this.currentSnapshotIndex = -1;
+        saveSnapshot();
+    }
+
+    private void saveSnapshot() {
+        while (currentSnapshotIndex < snapshot.size() - 1) {
+            snapshot.remove(snapshot.size() - 1);
+        }
+        snapshot.add(stringBuilder.toString());
+        currentSnapshotIndex++;
+    }
+
+    public boolean undo() {
+        if (currentSnapshotIndex > 0) {
+            currentSnapshotIndex--;
+            restoreSnapshot();
+            return true;
+        }
+        return false;
+    }
+
+    private void restoreSnapshot() {
+        stringBuilder = new StringBuilder(snapshot.get(currentSnapshotIndex));
+    }
+
+    public MyStringBuilder append(String string){
+        stringBuilder.append(string);
+        saveSnapshot();
+        return this;
+    }
+
+    public MyStringBuilder delete(int start, int end){
+        stringBuilder.delete(start, end);
+        saveSnapshot();
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return stringBuilder.toString();
+    }
+}
