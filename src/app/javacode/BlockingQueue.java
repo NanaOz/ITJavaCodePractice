973029be72d2a7260f8d42a65db6751258476f25
Alina <alinavik93@gmail.com>
2025-04-20 10:54:16 +0500

    Практическая задача - Concurrency - блокирующая очередь

--- src/app/javacode/BlockingQueue.java
+++ src/app/javacode/BlockingQueue.java
@@ -1,5 +1,6 @@
 package app.javacode;
 
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,7 +9,7 @@ public class BlockingQueue<T> {
     private final int capacity;
 
     public BlockingQueue(int capacity) {
-        this.queue = new LinkedList<T>();
+        this.queue = new ArrayDeque<>();
         this.capacity = capacity;
     }
 
