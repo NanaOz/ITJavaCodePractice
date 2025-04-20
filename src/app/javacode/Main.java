a9fec2c317633a0fabf66713fe0e42482a19dc95
Alina <alinavik93@gmail.com>
2025-04-20 10:43:09 +0500

    Практическая задача - Concurrency - блокирующая очередь

--- src/app/javacode/Main.java
+++ src/app/javacode/Main.java
@@ -1,7 +1,34 @@
 package app.javacode;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        BlockingQueue<String> queue = new BlockingQueue<>(10);
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    String item = "item" + i;
+                    queue.enqueue(item);
+                    System.out.println("enqueue: " + item);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    String item = queue.dequeue();
+                    System.out.println("dequeue: " + item);
+                    Thread.sleep(1800);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
     }
 }
