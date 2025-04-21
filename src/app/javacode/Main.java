2306daa6770f79cac0b2190a86885e3cc8a157b4
Alina <alinavik93@gmail.com>
2025-04-21 12:26:16 +0500

    Практическое задание - Stream API - генерация чисел

--- src/app/javacode/Main.java
+++ src/app/javacode/Main.java
@@ -1,7 +1,28 @@
 package app.javacode;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+
+
+        Map<String, Double> ordersStream = orders.stream()
+                .collect(Collectors.groupingBy(Order::getProduct, Collectors.summingDouble(Order::getCost)))
+                .entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(3)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        for (Map.Entry<String, Double> entry : ordersStream.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
     }
 }
