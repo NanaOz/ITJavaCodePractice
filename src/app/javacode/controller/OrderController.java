9f2026b05e95580e238c5a49612281161b45c0a5
Alina <alinavik93@gmail.com>
2025-04-29 16:21:02 +0500

    Практическое задание - Spring MVC - json view

--- src/app/javacode/controller/OrderController.java
+++ src/app/javacode/controller/OrderController.java
@@ -1,4 +1,47 @@
 package app.javacode.controller;
 
+import app.javacode.model.Order;
+import app.javacode.model.Views;
+import app.javacode.repository.OrderRepository;
+import app.javacode.repository.UserRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
 public class OrderController {
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+
+    public OrderController(OrderRepository orderRepository, UserRepository userRepository) {
+        this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/{userId}")
+    @JsonView(Views.OrderSummary.class)
+    public ResponseEntity<List<Order>> getUserOrders(@PathVariable Long userId) {
+        if(!userRepository.existsById(userId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<Order> orders = orderRepository.findByUserId(userId);
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{id}")
+    @JsonView(Views.OrderDetails.class)
+    public ResponseEntity<Order> getOrderById (@PathVariable Long id) {
+        Optional<Order> optionalOrder = orderRepository.findById(id);
+
+        if (optionalOrder.isPresent()) {
+            return ResponseEntity.ok(optionalOrder.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/app/javacode/controller/UserController.java
+++ src/app/javacode/controller/UserController.java
@@ -1,4 +1,49 @@
 package app.javacode.controller;
 
+import app.javacode.model.User;
+import app.javacode.model.Views;
+import app.javacode.repository.OrderRepository;
+import app.javacode.repository.UserRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+
+    public UserController(UserRepository userRepository, OrderRepository orderRepository) {
+        this.userRepository = userRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping
+    @JsonView(Views.UserSummary.class)
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setOrders(orderRepository.findByUserId(user.getId()));
+            return ResponseEntity.ok().body(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
