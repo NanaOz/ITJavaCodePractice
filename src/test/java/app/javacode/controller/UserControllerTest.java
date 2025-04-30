4223f74725760198f0ffa10d32a47dd9d37776aa
Alina <alinavik93@gmail.com>
2025-04-30 13:54:41 +0500

    Практическое задание - Spring MVC - json view

--- src/test/java/app/javacode/controller/UserControllerTest.java
+++ src/test/java/app/javacode/controller/UserControllerTest.java
@@ -1,70 +1,146 @@
 package app.javacode.controller;
 
-//import static org.mockito.Mockito.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//
-//import app.javacode.model.User;
-//import app.javacode.repository.OrderRepository;
-//import app.javacode.repository.UserRepository;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import java.util.Arrays;
-//import java.util.Optional;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import app.javacode.model.User;
+import app.javacode.repository.OrderRepository;
+import app.javacode.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 
 public class UserControllerTest {
-//
-//    private MockMvc mockMvc;
-//
-//    @Mock
-//    private UserRepository userRepository;
-//
-//    @Mock
-//    private OrderRepository orderRepository;
-//
-//    @InjectMocks
-//    private UserController userController;
-//
-//    @BeforeEach
-//    public void setup() {
-//        MockitoAnnotations.openMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-//    }
-//
-//    @Test
-//    public void testGetAllUsers_WithJsonView() throws Exception {
-//        User user1 = new User();
-//        user1.setId(1L);
-//        user1.setName("John Doe");
-//        user1.setEmail("john@example.com");
-//        User user2 = new User();
-//        user1.setId(2L);
-//        user1.setName("Bella M");
-//        user1.setEmail("bella@example.com");
-//
-//        when(userRepository.findAll()).thenReturn(Arrays.asList(user1, user2));
-//
-//        mockMvc.perform(get("/api/users")
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$[0].name").value("John Doe"))
-//                .andExpect(jsonPath("$[0].email").doesNotExist()) // в UserSummary не должно быть email
-//                .andExpect(jsonPath("$[1].name").value("Jane Doe"))
-//                .andExpect(jsonPath("$[1].email").doesNotExist());
-//    }
 
+    @Mock
+    private UserRepository userRepository;
 
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private UserController userController;
+
+    private MockMvc mockMvc;
+    private ObjectMapper mapper;
+    private User user;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        mapper = new ObjectMapper();
+
+        user = new User();
+        user.setId(1L);
+        user.setName("Anna A");
+        user.setEmail("anna@example.com");
+    }
+
+    @Test
+    public void getAllUsersShowUserSummary() throws Exception {
+        when(userRepository.findAll()).thenReturn(Arrays.asList(user));
+
+        mockMvc.perform(get("/api/users")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Anna A"))
+                .andExpect(jsonPath("$[0].email").value("anna@example.com"));
+    }
+
+    @Test
+    public void getUserByIdShouldShowUserDetails() throws Exception {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        mockMvc.perform(get("/api/users/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Anna A"))
+                .andExpect(jsonPath("$.email").value("anna@example.com"));
+    }
+
+    @Test
+    public void getUserByIdUserNotFounds() throws Exception {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/api/users/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void createUserShouldReturnCreatedUser() throws Exception {
+        User savedUser = new User();
+        savedUser.setId(1L);
+        savedUser.setName("Anna A");
+        savedUser.setEmail("anna@example.com");
+
+        when(userRepository.save(any(User.class))).thenReturn(savedUser);
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(user)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/users/" + user.getId()))
+                .andExpect(jsonPath("$.name").value("Anna A"));
+    }
+
+    @Test
+    public void updateUserShouldReturnUpdatedUser() throws Exception {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(userRepository.save(user)).thenReturn(user);
+
+        User updatedUser = new User();
+        updatedUser.setName("Anna B");
+        updatedUser.setEmail("annab@example.com");
+
+        mockMvc.perform(put("/api/users/" + user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Anna B"));
+    }
+
+    @Test
+    public void updateUserUserNotFound() throws Exception {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        User updatedUser = new User();
+        updatedUser.setName("Anna B");
+        updatedUser.setEmail("annab@example.com");
+
+        mockMvc.perform(put("/api/users" + user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedUser)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deleteUserShouldReturnDeletedUser() throws Exception {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        mockMvc.perform(delete("/api/users/" + user.getId()))
+                .andExpect(status().isNoContent());
+
+        verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    public void deleteUserUserNotFound() throws Exception {
+        when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/api/users/" + user.getId()))
+                .andExpect(status().isNotFound());
+    }
 }
