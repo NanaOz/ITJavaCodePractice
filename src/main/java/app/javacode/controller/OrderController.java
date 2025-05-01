3f80ae1e5292b3a7a4dada1c09f3db564568dbbc
Alina <alinavik93@gmail.com>
2025-05-01 20:53:23 +0500

    Практическое задание - MVC - Object mapper

--- src/main/java/app/javacode/controller/OrderController.java
+++ src/main/java/app/javacode/controller/OrderController.java
@@ -1,4 +1,27 @@
 package app.javacode.controller;
 
+import app.javacode.repository.CustomerRepository;
+import app.javacode.repository.OrderRepository;
+import app.javacode.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+    private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
+    private final ProductRepository productRepository;
+    private final ObjectMapper objectMapper;
+
+    public OrderController(OrderRepository orderRepository, CustomerRepository customerRepository, ProductRepository productRepository, ObjectMapper objectMapper) {
+        this.orderRepository = orderRepository;
+        this.customerRepository = customerRepository;
+        this.productRepository = productRepository;
+        this.objectMapper = objectMapper;
+    }
+
+
 }
--- src/main/java/app/javacode/controller/ProductController.java
+++ src/main/java/app/javacode/controller/ProductController.java
@@ -1,4 +1,78 @@
 package app.javacode.controller;
 
+import app.javacode.dto.ProductDTO;
+import app.javacode.model.Product;
+import app.javacode.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductRepository productRepository;
+    private final ObjectMapper objectMapper;
+
+    public ProductController(ProductRepository productRepository, ObjectMapper objectMapper) {
+        this.productRepository = productRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOs = new ArrayList<>();
+
+        for (Product product : products) {
+            ProductDTO productDTO = objectMapper.convertValue(product, ProductDTO.class);
+            productDTOs.add(productDTO);
+        }
+        return ResponseEntity.ok(productDTOs);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            ProductDTO productDTO = objectMapper.convertValue(product, ProductDTO.class);
+            return ResponseEntity.ok(productDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO) {
+        Product product = objectMapper.convertValue(productDTO, Product.class);
+        Product savedProduct = productRepository.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(objectMapper.convertValue(savedProduct, ProductDTO.class));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        if (!productRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Product product = objectMapper.convertValue(productDTO, Product.class);
+        product.setProductId(id);
+        Product updateProduct = productRepository.save(product);
+        return ResponseEntity.ok(objectMapper.convertValue(updateProduct, ProductDTO.class));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long id) {
+        if (!productRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        productRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
