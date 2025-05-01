1dae57565885f390e25b25e63eede6e03fc701a8
Alina <alinavik93@gmail.com>
2025-05-01 23:05:33 +0500

    Практическое задание - MVC - Object mapper

--- src/main/java/app/javacode/controller/CustomerController.java
+++ src/main/java/app/javacode/controller/CustomerController.java
@@ -1,4 +1,56 @@
 package app.javacode.controller;
 
+import app.javacode.dto.CustomerDTO;
+import app.javacode.model.Customer;
+import app.javacode.repository.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+    private final CustomerRepository customerRepository;
+    private final ObjectMapper objectMapper;
+
+    public CustomerController(CustomerRepository customerRepository, ObjectMapper objectMapper) {
+        this.customerRepository = customerRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDTO>> getAllCustumers() {
+        List<Customer> customers = customerRepository.findAll();
+        List<CustomerDTO> customerDTOList = new ArrayList<>();
+
+        for (Customer customer : customers) {
+            CustomerDTO customerDTO = objectMapper.convertValue(customer, CustomerDTO.class);
+            customerDTOList.add(customerDTO);
+        }
+        return ResponseEntity.ok(customerDTOList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDTO> getCustumerById(@PathVariable long id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if (customer.isPresent()) {
+            Customer customerEntity = customer.get();
+            CustomerDTO customerDTO = objectMapper.convertValue(customerEntity, CustomerDTO.class);
+            return ResponseEntity.ok(customerDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        Customer customer = objectMapper.convertValue(customerDTO, Customer.class);
+        Customer savedCustomer = customerRepository.save(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(objectMapper.convertValue(savedCustomer, CustomerDTO.class));
+    }
 }
--- src/main/java/app/javacode/controller/OrderController.java
+++ src/main/java/app/javacode/controller/OrderController.java
@@ -1,12 +1,20 @@
 package app.javacode.controller;
 
+import app.javacode.dto.OrderDTO;
+import app.javacode.model.Order;
+import app.javacode.model.Product;
 import app.javacode.repository.CustomerRepository;
 import app.javacode.repository.OrderRepository;
 import app.javacode.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -23,5 +31,46 @@ public class OrderController {
         this.objectMapper = objectMapper;
     }
 
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> getOrders() {
+        List<Order> orders = orderRepository.findAll();
+        List<OrderDTO> orderDTOs = new ArrayList<>();
 
+        for (Order order : orders) {
+            OrderDTO orderDTO = objectMapper.convertValue(order, OrderDTO.class);
+            orderDTOs.add(orderDTO);
+        }
+
+        return new ResponseEntity<>(orderDTOs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> getOrder(@PathVariable Long id) {
+        Optional<Order> optionalOrder = orderRepository.findById(id);
+        if (optionalOrder.isPresent()) {
+            Order order = optionalOrder.get();
+            OrderDTO orderDTO = objectMapper.convertValue(order, OrderDTO.class);
+            return ResponseEntity.ok(orderDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) {
+        if(!customerRepository.existsById(orderDTO.getCustomer().getCustomerId())){
+            return ResponseEntity.notFound().build();
+        }
+
+        for (Product product : orderDTO.getProducts()) {
+            if(!productRepository.existsById(product.getProductId())){
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        Order order = objectMapper.convertValue(orderDTO, Order.class);
+        order.setOrderDate(LocalDate.now());
+        Order savedOrder = orderRepository.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(objectMapper.convertValue(savedOrder, OrderDTO.class));
+    }
 }
